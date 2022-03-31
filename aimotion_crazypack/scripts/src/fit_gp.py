d66312a5a62eeddeac56c56fc78761f0a22ee902
AIMotionLab <aimotionlab.sztaki@gmail.com>
2022-03-31 16:15:56 +0200

    Update plotting, add log files

--- aimotion_crazypack/scripts/src/fit_gp.py
+++ aimotion_crazypack/scripts/src/fit_gp.py
@@ -57,6 +57,7 @@ def read_log_from_tau_usd(filename="../usdlog/log16.csv"):
     res["My"] = My
     res["t"] = np.array(df["tick"]) / 1000
     res["ind"] = np.array(df["ctrlGeom.cmd_roll"])
+    res["psi"] = np.array(df["ctrlGeom.psi"])
 
     return res
 
@@ -86,11 +87,16 @@ class ExactGPModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
 rp_scale = 132000*4/0.0325
 yaw_scale = 132000*4/0.025
 # res1 = read_log_from_tau(filename="/home/crazyfly/.ros/save/logcf4_train.csv")
 # res = read_log_from_tau(filename="/home/crazyfly/.ros/logcf4.csv")
-res = read_log_from_tau_usd(filename="/media/crazyfly/UNTITLED/log05")
+res = read_log_from_tau_usd(filename="/media/crazyfly/UNTITLED/log23")
 # res2 = read_log_from_tau(filename="/home/crazyfly/.ros/logcf4.csv")
 # res2 = read_log_from_tau(filename="/home/crazyfly/.ros/save/logcf4_train2.csv")
 # res = {}
@@ -106,94 +112,106 @@ for i in range(3):
     ang_accel[i, :] = np.gradient(ang_vel[i, :], 0.004)
 M2 = np.zeros_like(ang_accel)
 torque = np.vstack((Mx, My, np.zeros_like(Mx)))
+torque = np.roll(torque, 14, 1)
+for i in range(3):
+    torque[i, 3:] = moving_average(torque[i, :], 4)
 for i in range(ang_accel.shape[1]):
     M2[:, i] = inertia @ ang_accel[:, i] + np.cross(ang_vel[:, i], inertia @ ang_vel[:, i])
 
 # plt.figure()
 # plt.plot(res["omega_y"][indices], torque.T - M2.T)
-plt.figure()
-plt.plot(res["t"][indices], M2.T[:, 1])
-plt.plot(res["t"][indices], torque.T[:, 1])
-plt.show()
+# t = res["t"][indices]
+# t = t - t[0]
+# plt.figure()
+# ind = 1
+# plt.plot(t, M2.T[:, ind]*1000)
+# plt.plot(t, torque.T[:, ind]*1000)
+# plt.plot(t, -M2.T[:, ind]*1000 + torque.T[:, ind]*1000)
+# # plt.plot(t, res["psi"][indices])
+# plt.grid(True)
+# plt.xlabel("t")
+# plt.ylabel("Torque (mNm)")
+# plt.legend(("Computed torque", "Commanded torque", "Difference"))
+# plt.show()
 
-# train_x = np.vstack((res["qx"][indices], res["qy"][indices], res["omega_x"][indices]/10000, res["omega_y"][indices]/10000)).T
-# train_y = [(torque - M2)[0, :].T, (torque - M2)[1, :].T]
-# train_y = [train_yi * 200 + 0.01 * np.random.randn(train_yi.shape[0]) for train_yi in train_y]
+train_x = np.vstack((res["qx"][indices], res["qy"][indices], res["omega_x"][indices]/10000, res["omega_y"][indices]/10000)).T
+train_y = [(torque - M2)[0, :].T, (torque - M2)[1, :].T]
+train_y = [train_yi * 200 + 0.01 * np.random.randn(train_yi.shape[0]) for train_yi in train_y]
+
+train_x = torch.from_numpy(train_x).float()
+train_y = [torch.from_numpy(train_yi).float() for train_yi in train_y]
+
+likelihood = [gpytorch.likelihoods.GaussianLikelihood() for _ in range(2)]
+model = [ExactGPModel(train_x, train_y[i], likelihood[i]) for i in range(2)]
+
+training_iter = 200
+# Find optimal model hyperparameters
+for i in range(2):
+    model[i].train()
+    likelihood[i].train()
+
+    # Use the adam optimizer
+    optimizer = torch.optim.Adam(model[i].parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+    # "Loss" for GPs - the marginal log likelihood
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood[i], model[i])
+
+    for iter in range(training_iter):
+        # Zero gradients from previous iteration
+        optimizer.zero_grad()
+        # Output from model
+        output = model[i](train_x)
+        # Calc loss and backprop gradients
+        loss = -mll(output, train_y[i])
+        loss.backward()
+        # print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+        #     iter + 1, training_iter, loss.item(),
+        #     model[i].covar_module.base_kernel.lengthscale.item(),
+        #     model[i].likelihood.noise.item()
+        # ))
+        # print('Iter %d/%d - Loss: %.3f   noise: %.3f' % (
+        #     iter + 1, training_iter, loss.item(),
+        #     model[i].likelihood.noise.item()
+        # ))
+        optimizer.step()
+    print(loss.item())
+
+# sf2 = [m.covar_module.outputscale.detach().numpy() for m in model]
+# lscale = [m.covar_module.base_kernel.lengthscale.detach().numpy() for m in model]
+# se2 = [m.likelihood.noise.detach().numpy()**2 for m in model]
+# X = train_x.numpy()[::3]
+# y = [train_yi.numpy()[::3] for train_yi in train_y]
+# K = [np.zeros((X.shape[0], X.shape[0])) for _ in range(2)]
+# for elem, K1 in enumerate(K):
+#     for i in range(K1.shape[0]):
+#         for j in range(K1.shape[0]):
+#             K1[i, j] = RBFkernel(X[i, :], X[j, :], lscale[elem][0], sf2[elem])
+# alpha = [np.linalg.inv(K1+se2[elem]*np.eye(K1.shape[0])) @ y[elem] for elem, K1 in enumerate(K)]
+
+# print("float X[48][4] = " + printC(X))
+# print("float alpha1[48] = " + printC(alpha[0]))
+# print("float alpha2[48] = " + printC(alpha[1]))
+# print("float lam1[4] = " + printC(1/lscale[0]**2))
+# print("float lam2[4] = " + printC(1/lscale[1]**2))
+
+[m.eval() for m in model]
+[l.eval() for l in likelihood]
+num_x = 5
+num_y = 9
+sum_num = num_x**2 * num_y**2
+qxs = torch.linspace(-0.05, 0.05, num_x)
+qys = torch.linspace(-1, 0.2, num_y)
+wxs = torch.linspace(-0.2, 0.2, num_x)
+wys = torch.linspace(-0.2, 2.3, num_y)
+qxs, qys, wxs, wys = torch.meshgrid(qxs, qys, wxs, wys)
+qxs, qys, wxs, wys = torch.reshape(qxs, (sum_num, 1)), torch.reshape(qys, (sum_num, 1)), \
+                     torch.reshape(wxs, (sum_num, 1)), torch.reshape(wys, (sum_num, 1)),
+
+test_x = torch.cat((qxs, qys, wxs, wys), 1)
+observed_pred = [likelihood[i](model[i](test_x)) for i in range(2)]
+test_y = [torch.reshape(pred.mean, (num_x, num_y, num_x, num_y)) for pred in observed_pred]
+csv_arr = [torch.reshape(test_yi, (sum_num, 1)) for test_yi in test_y]
+# csv_write = 10000*csv_arr[0].detach().numpy().T
+np.savetxt('ltb.csv', 100000*csv_arr[1].detach().numpy().T, "%i", delimiter=',')
 #
-# train_x = torch.from_numpy(train_x).float()
-# train_y = [torch.from_numpy(train_yi).float() for train_yi in train_y]
-#
-# likelihood = [gpytorch.likelihoods.GaussianLikelihood() for _ in range(2)]
-# model = [ExactGPModel(train_x, train_y[i], likelihood[i]) for i in range(2)]
-#
-# training_iter = 200
-# # Find optimal model hyperparameters
-# for i in range(2):
-#     model[i].train()
-#     likelihood[i].train()
-#
-#     # Use the adam optimizer
-#     optimizer = torch.optim.Adam(model[i].parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
-#
-#     # "Loss" for GPs - the marginal log likelihood
-#     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood[i], model[i])
-#
-#     for iter in range(training_iter):
-#         # Zero gradients from previous iteration
-#         optimizer.zero_grad()
-#         # Output from model
-#         output = model[i](train_x)
-#         # Calc loss and backprop gradients
-#         loss = -mll(output, train_y[i])
-#         loss.backward()
-#         # print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
-#         #     iter + 1, training_iter, loss.item(),
-#         #     model[i].covar_module.base_kernel.lengthscale.item(),
-#         #     model[i].likelihood.noise.item()
-#         # ))
-#         # print('Iter %d/%d - Loss: %.3f   noise: %.3f' % (
-#         #     iter + 1, training_iter, loss.item(),
-#         #     model[i].likelihood.noise.item()
-#         # ))
-#         optimizer.step()
-#     print(loss.item())
-#
-# # sf2 = [m.covar_module.outputscale.detach().numpy() for m in model]
-# # lscale = [m.covar_module.base_kernel.lengthscale.detach().numpy() for m in model]
-# # se2 = [m.likelihood.noise.detach().numpy()**2 for m in model]
-# # X = train_x.numpy()[::3]
-# # y = [train_yi.numpy()[::3] for train_yi in train_y]
-# # K = [np.zeros((X.shape[0], X.shape[0])) for _ in range(2)]
-# # for elem, K1 in enumerate(K):
-# #     for i in range(K1.shape[0]):
-# #         for j in range(K1.shape[0]):
-# #             K1[i, j] = RBFkernel(X[i, :], X[j, :], lscale[elem][0], sf2[elem])
-# # alpha = [np.linalg.inv(K1+se2[elem]*np.eye(K1.shape[0])) @ y[elem] for elem, K1 in enumerate(K)]
-#
-# # print("float X[48][4] = " + printC(X))
-# # print("float alpha1[48] = " + printC(alpha[0]))
-# # print("float alpha2[48] = " + printC(alpha[1]))
-# # print("float lam1[4] = " + printC(1/lscale[0]**2))
-# # print("float lam2[4] = " + printC(1/lscale[1]**2))
-#
-# [m.eval() for m in model]
-# [l.eval() for l in likelihood]
-# num_x = 5
-# num_y = 9
-# sum_num = num_x**2 * num_y**2
-# qxs = torch.linspace(-0.05, 0.05, num_x)
-# qys = torch.linspace(-1, 0.2, num_y)
-# wxs = torch.linspace(-0.2, 0.2, num_x)
-# wys = torch.linspace(-0.2, 2.3, num_y)
-# qxs, qys, wxs, wys = torch.meshgrid(qxs, qys, wxs, wys)
-# qxs, qys, wxs, wys = torch.reshape(qxs, (sum_num, 1)), torch.reshape(qys, (sum_num, 1)), \
-#                      torch.reshape(wxs, (sum_num, 1)), torch.reshape(wys, (sum_num, 1)),
-#
-# test_x = torch.cat((qxs, qys, wxs, wys), 1)
-# observed_pred = [likelihood[i](model[i](test_x)) for i in range(2)]
-# test_y = [torch.reshape(pred.mean, (num_x, num_y, num_x, num_y)) for pred in observed_pred]
-# csv_arr = [torch.reshape(test_yi, (sum_num, 1)) for test_yi in test_y]
-# # csv_write = 10000*csv_arr[0].detach().numpy().T
-# np.savetxt('ltb.csv', 10000*csv_arr[0].detach().numpy().T, "%i", delimiter=',')
-# #
-# # ltb = np.loadtxt('ltb.csv', delimiter=',')
+# ltb = np.loadtxt('ltb.csv', delimiter=',')
--- aimotion_crazypack/scripts/usdlog/plot_usd.py
+++ aimotion_crazypack/scripts/usdlog/plot_usd.py
@@ -11,6 +11,13 @@ import re
 import argparse
 import numpy as np
 
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+def filter(a):
+    return np.hstack((moving_average(a, 5), 0, 0, 0, 0))
 
 def read_log(filename='/media/crazyfly/UNTITLED/log00'):
     # decode binary log data
@@ -20,28 +27,64 @@ def read_log(filename='/media/crazyfly/UNTITLED/log00'):
     res["omega_y"] = np.array(df["stateEstimateZ.ratePitch"])
     res["qx"] = np.array(df["stateEstimate.qx"])
     res["qy"] = np.array(df["stateEstimate.qy"])
-    res["Mx"] = np.array(df["ctrlFlip.cmd_roll"])
-    res["My"] = np.array(df["ctrlFlip.cmd_pitch"])
-    res["u0"] = np.array(df["ctrlGeom.u0"])
-    res["u1"] = np.array(df["ctrlGeom.u1"])
-    res["psi"] = np.array(df["ctrlFlip.psi"])
+    res["Mx"] = filter(np.array(df["ctrlFlip.cmd_roll"])/(132/0.0325))
+    res["My"] = filter(np.array(df["ctrlFlip.cmd_pitch"])/(132/0.0325))
+    res["u0"] = np.array(df["ctrlGeom.u0"])*1000
+    res["u1"] = np.array(df["ctrlGeom.u1"])*1000
+    res["psi"] = filter(np.array(df["ctrlGeom.psi"]))
     res["ind"] = np.array(df["ctrlGeom.cmd_roll"])
     res["t"] = np.array(df["tick"]) / 1000
+    res["x"] = filter(np.array(df["stateEstimate.x"]))
+    res["y"] = filter(np.array(df["stateEstimate.y"]))
     return res
 
 
-res = read_log('/media/crazyfly/UNTITLED/log05')
-indices = [np.abs(grad) > 1e-7 for grad in np.gradient(res["ind"])]
+res1 = read_log('log_no_gp')
+res2 = read_log('log_with_gp')
+indices1 = [np.abs(grad) > 1e-7 for grad in np.gradient(res1["ind"])]
+indices2 = [np.abs(grad) > 1e-7 for grad in np.gradient(res2["ind"])]
 rp_scale = 132000 * 4 / 0.0325
 
-t = res["t"][indices]
-t = t - t[0]
+t1 = res1["t"][indices1]
+t1 = t1 - t1[0]
+t2 = res2["t"][indices2]
+t2 = t2 - t2[0]
 fig, axs = plt.subplots(5)
-axs[0].plot(t, res["Mx"][indices])
-axs[1].plot(t, res["My"][indices])
-axs[2].plot(t, res["u0"][indices])
-axs[3].plot(t, res["u1"][indices])
-axs[4].plot(t, res["psi"][indices])
+
+axs[0].plot(t1, res1["Mx"][indices1])
+axs[1].plot(t1, res1["My"][indices1])
+axs[2].plot(t1, res1["u0"][indices1])
+axs[3].plot(t1, res1["u1"][indices1])
+axs[4].plot(t1[1:], res1["psi"][indices1][1:])
+# axs[5].plot(t1, res1["x"][indices1])
+# axs[5].plot(t1, res1["y"][indices1])
+
+axs[0].plot(t2, res2["Mx"][indices2])
+axs[1].plot(t2, res2["My"][indices2])
+axs[2].plot(t2, res2["u0"][indices2])
+axs[3].plot(t2, res2["u1"][indices2])
+axs[4].plot(t2[1:], res2["psi"][indices2][1:])
+# axs[5].plot(t2, res2["x"][indices2])
+# axs[5].plot(t2, res2["y"][indices2])
+
+[axs_.set_xlabel("$t$") for axs_ in axs]
+axs[0].set_ylabel("$M_x$")
+axs[1].set_ylabel("$M_y$")
+axs[2].set_ylabel("$\hat\eta_x$")
+axs[3].set_ylabel("$\hat\eta_y$")
+axs[4].set_ylabel("$\Psi$")
+
+axs[0].legend(('Without GP', 'With GP mean'), loc='upper left')
+
+fig.subplots_adjust(left=0.125,
+                    bottom=0.1,
+                    right=0.99,
+                    top=0.99,
+                    hspace=0.8
+                    )
+[ax.grid(True) for ax in axs]
+plt.show()
+
 
 '''
 res = read_log('~/.ros/save/logcf4_gp11.csv')
@@ -65,4 +108,3 @@ axs[4].set_ylim([0, 0.1])
 axs[4].set_ylabel('$\Psi$')
 '''
 
-[ax.grid(True) for ax in axs]
